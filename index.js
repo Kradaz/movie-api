a4dca186e7c00875f81f1d00bddf64638797101e
Kradaz <adarshsaulick@hotmail.it>
2022-10-23 17:02:34 +0100

    updated connections

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const Models = require('./models.js');
 const Movies = Models.Movie;
 const Users = Models.User;
 
-mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect('mongodb://localhost:27017/FlixApp', { useNewUrlParser: true, useUnifiedTopology: true });
 
   app.use(bodyParser.json());
 
