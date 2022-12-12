ca385297587699f605af84572d5a2c3d8c28e3d2
Kradaz <adarshsaulick@hotmail.it>
2022-12-12 18:37:37 +0000

    fix
    rearranged mongoose connect

--- index.js
+++ index.js
@@ -11,7 +11,10 @@ const Movies = Models.Movie;
 const Users = Models.User;
 
 //mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true, useUnifiedTopology: true })
-mongoose.connect( process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.CONNECTION_URI, {
+ useNewUrlParser: true,
+ useUnifiedTopology: true
+});
 
   app.use(bodyParser.json());
 
