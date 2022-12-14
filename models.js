6627c172aa3e400a169182edfdeabc5b8e367934
Kradaz <adarshsaulick@hotmail.it>
2022-12-14 17:38:25 +0000

    has fixed
    missing H on the spelling

--- models.js
+++ models.js
@@ -26,7 +26,7 @@ let movieSchema = mongoose.Schema({
   });
 
   userSchema.statics.hashPassword = (password) => {
-    return bcrypt.hasSync(password, 10);
+    return bcrypt.hashSync(password, 10);
   };
 
   userSchema.methods.validatePassword = function(password) {
